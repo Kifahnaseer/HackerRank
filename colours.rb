83728a1829017b399173c96b87ef69d31a02e60a
kifahnaseer <kifahnaeer28@gmail.com>
2023-02-09 13:02:41 +0300

    solutions for question

---
+++ colours.rb
@@ -0,0 +1,11 @@
+def iterate_colors(colors)
+    # Your code here
+     result = []
+     colors.each do |color|
+      result << color
+    end
+    result
+  end
+
+ 
+   puts  iterate_colors(["red", "green", "blue"])
